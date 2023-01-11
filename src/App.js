10ef0e1a6a4c95cac293f6bfa1ee55b7c8c0dc1b
Abhishek Thakur <abhishekthakur.art@protonmail.com>
2023-01-10 19:16:20 -0500

    added http

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
           <h1 className="display-3">WELCOME TO BOOTCAMP - Jan 10 2023 Edition</h1>
           <p className="lead">My Name is Abhishek Thakur and I am Student  </p>
           <p className="lead"><a href="https://github.com/iamabhishekt">Here</a> is my Github Link  </p>
-          <p className="lead"><a href="http://webdevnotes.wynisco.com">Here</a> Here is Class Notes Link </p>
-          <p className="lead"><a href="webdevhw.wynisco.com">Here</a> Here is Homeworks Link </p>
+          <p className="lead"><a href="https://webdevnotes.wynisco.com">Here</a> Here is Class Notes Link </p>
+          <p className="lead"><a href="https://webdevhw.wynisco.com">Here</a> Here is Homeworks Link </p>
           <p className="lead"><a href="https://docs.google.com/spreadsheets/d/1I75Kt78QSCRSAiF74yBttNyWVI4bq2ONsTWnN_ai9iA/edit?usp=sharing">Here</a> Main Google Spreadsheet </p>
 
      <DataTable />

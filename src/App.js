60e7bfbdccda5d00bf48ae6870d5af630e3dd0ac
Abhishek Thakur <abhishekthakur.art@protonmail.com>
2023-01-10 19:08:22 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import DataTable from './DataTable' ;
+import { Container} from 'reactstrap';
+
+
+
+function App() {
+  return (
+    <div className="App">
+ <Container fluid>
+          <h1 className="display-3">WELCOME TO BOOTCAMP - Jan 10 2023 Edition</h1>
+          <p className="lead">My Name is Abhishek Thakur and I am Student  </p>
+          <p className="lead"><a href="https://github.com/iamabhishekt">Here</a> is my Github Link  </p>
+          <p className="lead"><a href="http://webdevnotes.wynisco.com">Here</a> Here is Class Notes Link </p>
+          <p className="lead"><a href="webdevhw.wynisco.com">Here</a> Here is Homeworks Link </p>
+          <p className="lead"><a href="https://docs.google.com/spreadsheets/d/1I75Kt78QSCRSAiF74yBttNyWVI4bq2ONsTWnN_ai9iA/edit?usp=sharing">Here</a> Main Google Spreadsheet </p>
+
+     <DataTable />
+     </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DataTable.jsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(
+  name: string,
+  codelink: string,
+  deployedLink: string,
+  carbs: number,
+  protein: number,
+) {
+  return { name, codelink,deployedLink };
+}
+
+const rows = [
+  createData('Homework 1 Git Commands Test', "https://github.com/sachinrajgire", "codepen.com/ci=sdfdfdsf" ),
+  
+];
+
+export default function DenseTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Homework Name</TableCell>
+            <TableCell align="right">Github ( If Applicable)</TableCell>
+            <TableCell align="right">Deployed Link (If Applicable )</TableCell>
+            {/* <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}

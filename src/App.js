576f9f99eded21c55641e9e30a14e9bde2dd0cfa
Abhishek Thakur <abhishekthakur.art@protonmail.com>
2023-01-11 00:55:01 -0500

    changes and added hw0

--- src/App.js
+++ src/App.js
@@ -9,20 +9,19 @@ function App() {
         <h1 className="display-3">WELCOME TO BOOTCAMP - Jan 10 2023 Edition</h1>
         <p className="lead">My Name is Abhishek Thakur and I am a Student</p>
         <p className="lead">
-          <a href="http://github.com/iamabhishekt">Here</a> is my Github Link{" "}
+          Here is my Github <a href="http://github.com/iamabhishekt">Link </a>
         </p>
         <p className="lead">
-          <a href="http://webdevnotes.wynisco.com">Here</a> Here is Class Notes
-          Link{" "}
+          Here is Class Notes <a href="http://webdevnotes.wynisco.com">Link </a>
         </p>
         <p className="lead">
-          <a href="http://webdevhw.wynisco.com">Here</a> Here is Homeworks Link{" "}
+          Here is Homeworks <a href="http://webdevhw.wynisco.com">Link </a>
         </p>
         <p className="lead">
-          <a href="http://docs.google.com/spreadsheets/d/1I75Kt78QSCRSAiF74yBttNyWVI4bq2ONsTWnN_ai9iA/edit?usp=sharing">
-            Here
-          </a>{" "}
           Main Google Spreadsheet{" "}
+          <a href="http://docs.google.com/spreadsheets/d/1I75Kt78QSCRSAiF74yBttNyWVI4bq2ONsTWnN_ai9iA/edit?usp=sharing">
+            Link{" "}
+          </a>
         </p>
         <DataTable />
       </Container>
--- src/DataTable.jsx
+++ src/DataTable.jsx
@@ -1,25 +1,22 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
-function createData(
-  name: string,
-  codelink: string,
-  deployedLink: string,
-  carbs: number,
-  protein: number,
-) {
-  return { name, codelink,deployedLink };
+function createData(name: string, codelink: string, deployedLink: string) {
+  return { name, codelink, deployedLink };
 }
 
 const rows = [
-  createData('Homework 1 Git Commands Test', "https://github.com/sachinrajgire", "codepen.com/ci=sdfdfdsf" ),
-  
+  createData(
+    "(Pre-Bootcamp) Homework 0 Flex-box and Grid with Responsiveness",
+    "https://github.com/iamabhishekt/responsive_exercise",
+    "https://responsive-layout-exercise.netlify.app"
+  ),
 ];
 
 export default function DenseTable() {
@@ -31,23 +28,23 @@ export default function DenseTable() {
             <TableCell>Homework Name</TableCell>
             <TableCell align="right">Github ( If Applicable)</TableCell>
             <TableCell align="right">Deployed Link (If Applicable )</TableCell>
-            {/* <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
               key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">
+                <a href={row.codelink}>Github Link</a>
+              </TableCell>
+              <TableCell align="right">
+                <a href={row.deployedLink}>Netlify Link</a>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
